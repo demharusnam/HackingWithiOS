824377466f8e9e3cec5a656ec634f49b57c456cc
Mansur Ahmed <ahmedm.dev@gmail.com>
2020-04-14 16:53:52 -0400

    complete project 7: iExpense

---
+++ SwiftUI/iExpense/iExpense/AddView.swift
@@ -0,0 +1,55 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Mansur Ahmed on 2020-04-14.
+//  Copyright © 2020 Mansur Ahmed. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var expenses: Expenses
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    @State private var isShowing = false
+    
+    static let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add New Expense")
+            .navigationBarItems(trailing: Button("Save") {
+                if let actualAmount = Int(self.amount) {
+                    let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
+                    self.expenses.items.append(item)
+                    self.presentationMode.wrappedValue.dismiss()
+                } else { self.isShowing = true }
+            })
+            .alert(isPresented: self.$isShowing) {
+                Alert(title: Text("Error"), message: Text("Please input an integer value for the amount field."), dismissButton: Alert.Button.default(Text("OK")))
+            }
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
